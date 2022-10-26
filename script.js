39db494d509133956edfedc000209870dad99479
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2022-10-26 19:48:02 -0300

    pegando respostas e as modificando com fetch, capturando erros com catch e utilizando o finally

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const consultaCep = fetch(`https://viacep.com.br/ws/01001000/json/`)
+  .then(response => response.json())
+  .then(r => {
+    if (r.erro) {
+      throw new Error('Esse cep não existe')
+    } else {
+      console.log(r)
+    }
+  })
+  .catch(erro => console.log(erro))
+  .finally(mensagem => console.log('Processamento comcluido'))
