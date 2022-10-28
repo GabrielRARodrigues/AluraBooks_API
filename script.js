49aab17c2fab421f17d139c4540ab8b5dcf7876e
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2022-10-28 20:27:18 -0300

    Terminando o projeto, preenchendo os campos de endereço com os dados advindos do resultado da requisição para a API viaCep usando os cep colocado no formulario, imprimindo no hmtl uma mensagem de erro caso CEP inválido, e refatorando código

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const cidade = document.getElementById('cidade')
+
+const logradouro = document.getElementById('endereco')
+
+const estado = document.getElementById('estado')
+
+const mensagemErro = document.getElementById('erro')
+
+const cep = document.getElementById('cep')
+cep.addEventListener('focusout', () => buscaEndereco(cep.value))
+
 async function buscaEndereco(cep) {
   try {
     const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
@@ -5,11 +16,20 @@ async function buscaEndereco(cep) {
     if (consultaCEPConvertida.erro) {
       throw Error('CEP não existente')
     }
+    preencheCamposEndereco(consultaCEPConvertida)
     return consultaCEPConvertida
   } catch (erro) {
+    mensagemErro.innerHTML = `<p style="margin:10px; color:#D2001A;">CEP inválido. Tente novamente!</p>`
     console.log(erro)
   }
 }
+
+function preencheCamposEndereco(endereco) {
+  cidade.value = endereco.localidade
+  logradouro.value = endereco.logradouro
+  estado.value = endereco.uf
+}
+
 // let ceps = ['01001000', '01001001']
 // let conjuntosCeps = ceps.map(valores => buscaEndereco(valores))
 // Promise.all(conjuntosCeps).then(respostas => console.log(respostas))
